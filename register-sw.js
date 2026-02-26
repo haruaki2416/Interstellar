f94a9740ba4b82c312f6616eaa732cac9ac86523
haruaki2416 <haruakinahidwin@gmail.com>
2026-02-26 10:32:27 +0900

    Update register-sw.js

--- register-sw.js
+++ register-sw.js
@@ -1,1 +1,6 @@
-
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js', { scope: '/' })
+      .catch(err => console.error('SW registration failed:', err));
+  });
+}
