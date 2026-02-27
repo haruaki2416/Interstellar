14fae6c2bfeaea6ec76b9fa8f4be0b3733f01716
haruaki2416 <haruakinahidwin@gmail.com>
2026-02-27 09:22:53 +0900

    Update config.js

--- scramjet/config.js
+++ scramjet/config.js
@@ -1,1 +1,9 @@
-let _CONFIG = {};
+self.__uv$config = {
+  prefix: '/scramjet/g/',
+  bare: '/bare/',
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler: '/scramjet/uv.handler.js',
+  bundle: '/scramjet/uv.bundle.js',
+  config: '/scramjet/uv.config.js',
+};
